1ad2ffcea872b67e788267ddb3c888ec410758b8
Guilherme Rodrigues <guilodron7@gmail.com>
2020-07-21 18:25:19 -0300

    Adiciona rotas na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import {BrowserRouter} from 'react-router-dom';
 
 import Header from './components/Header';
 import GlobalStyle from './globalStyle';
 import Footer from './components/Footer';
+import Routes from './Routes';
 
 function App() {
   return (
     <>
-      <Header />
       <GlobalStyle />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
