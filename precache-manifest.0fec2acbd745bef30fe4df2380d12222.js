61275b73c8ef058088d6f23224ae75bc0577ae4f
Guilherme Rodrigues <guilodron7@gmail.com>
2020-07-23 18:34:45 -0300

    Updates

---
+++ precache-manifest.0fec2acbd745bef30fe4df2380d12222.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9abc16c0d48442b0fa71db945fe39b0b",
+    "url": "/petshop-frontend/index.html"
+  },
+  {
+    "revision": "50587908defbb88a270b",
+    "url": "/petshop-frontend/static/js/2.de40f0e7.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/petshop-frontend/static/js/2.de40f0e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63bb7966e21983f15dad",
+    "url": "/petshop-frontend/static/js/main.6be34491.chunk.js"
+  },
+  {
+    "revision": "a1dd0a648a644eecbc92",
+    "url": "/petshop-frontend/static/js/runtime-main.15359398.js"
+  },
+  {
+    "revision": "fd1f6bccb4afe32a0e1d3f79d163876b",
+    "url": "/petshop-frontend/static/media/loginImage.fd1f6bcc.svg"
+  },
+  {
+    "revision": "106a908e98c504b15a73994e4d5044b2",
+    "url": "/petshop-frontend/static/media/signUpImage.106a908e.svg"
+  }
+]);
