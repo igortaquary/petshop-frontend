bc3b19227dc4a58244c5bbca2064e3cdf3693bae
Guilherme Rodrigues <guilodron7@gmail.com>
2020-07-24 00:46:02 -0300

    Updates

---
+++ precache-manifest.8032bf1f84e2892a2bb79ccd6fff6145.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2026773da572662bcf9fa8fe44bbc5a4",
+    "url": "/petshop-frontend/index.html"
+  },
+  {
+    "revision": "5994a81f637a2cb4c1a2",
+    "url": "/petshop-frontend/static/js/2.fcbfc893.chunk.js"
+  },
+  {
+    "revision": "66c7236c3751fcb8e7dfb6d8983314fd",
+    "url": "/petshop-frontend/static/js/2.fcbfc893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3fcebbb6b9e69e06025",
+    "url": "/petshop-frontend/static/js/main.117beb4c.chunk.js"
+  },
+  {
+    "revision": "a1dd0a648a644eecbc92",
+    "url": "/petshop-frontend/static/js/runtime-main.15359398.js"
+  },
+  {
+    "revision": "42773b4d578714b8e86960aff6d6b1a3",
+    "url": "/petshop-frontend/static/media/full3.42773b4d.webp"
+  },
+  {
+    "revision": "fd1f6bccb4afe32a0e1d3f79d163876b",
+    "url": "/petshop-frontend/static/media/loginImage.fd1f6bcc.svg"
+  },
+  {
+    "revision": "8941e99b86ba83d4a2f6d0845842931a",
+    "url": "/petshop-frontend/static/media/mainCachorro.8941e99b.png"
+  },
+  {
+    "revision": "c6333edfda752f8062de389853f70088",
+    "url": "/petshop-frontend/static/media/service1.c6333edf.png"
+  },
+  {
+    "revision": "d34082973a8057285d1d7c4bc91f905a",
+    "url": "/petshop-frontend/static/media/showerService.d3408297.png"
+  },
+  {
+    "revision": "106a908e98c504b15a73994e4d5044b2",
+    "url": "/petshop-frontend/static/media/signUpImage.106a908e.svg"
+  },
+  {
+    "revision": "9eb43cd956f4917b726ac599d901b240",
+    "url": "/petshop-frontend/static/media/trainingService.9eb43cd9.png"
+  },
+  {
+    "revision": "a0009c2db294d637baf014fdbd12b8a8",
+    "url": "/petshop-frontend/static/media/vetService.a0009c2d.png"
+  }
+]);
